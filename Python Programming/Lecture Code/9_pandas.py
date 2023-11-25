e2a8a2b3b24eb42ed2a1e526912577e63937b69c
iDolphin99 <idolphin.kr@gmail.com>
2023-11-25 23:28:51 +0900

    Refactor file and folder

--- Python Programming/Lecture Code/9_pandas.py
+++ Python Programming/Lecture Code/9_pandas.py
@@ -1,3 +1,10 @@
 import pandas as pd
+import numpy as np
 
-pd.read_excel('address.xlsx')
+df = pd.read_excel('address.xlsx')
+print(df['이름'])
+print(df.to_numpy())
+
+a = np.random.random((5,5))
+print(a)
+print(a[1:-1, 1:4])
