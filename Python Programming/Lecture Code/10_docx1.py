763325395d884eaf069acbebd169059d75b0ca97
iDolphin99 <idolphin.kr@gmail.com>
2023-12-15 15:00:34 +0900

    Add docx lecture code

---
+++ Python Programming/Lecture Code/10_docx1.py
@@ -0,0 +1,10 @@
+# pip install python-docx
+# pip install docx-mailmerge
+# pip install pywin32
+
+from docx import Document
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Mm
+
+doc = Document()
+
